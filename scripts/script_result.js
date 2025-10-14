9a8349fe5f6e3d9d5c785f341abb55222d876e4f
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-10-14 14:39:15 +0200

    corrections

--- scripts/script_result.js
+++ scripts/script_result.js
@@ -1,18 +1,5 @@
 window.onload = function () {
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
   const punteggio = localStorage.getItem("punteggioFinale");
   const totPunteggio = document.getElementById("div_punti");
   totPunteggio.innerHTML = `<p>Correct: ${punteggio}</p>`;
 };
-<<<<<<< Updated upstream
-=======
-    const punteggio = localStorage.getItem("punteggioFinale");
-    const totPunteggio = document.getElementById("div_punti");
-    totPunteggio.innerHTML = `<p>Punteggio: ${punteggio}</p>`;
-};
->>>>>>> main
-=======
->>>>>>> Stashed changes
