bdfa5af811da242d08b1686abd473552723d1ef4
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-10-15 08:56:38 +0200

    Update question_page.js

--- scripts/question_page.js
+++ scripts/question_page.js
@@ -31,120 +31,115 @@
 */
 
 const questions = [
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "What does CPU stand for?",
-        correct_answer: "Central Processing Unit",
-        incorrect_answers: [
-            "Central Process Unit",
-            "Computer Personal Unit",
-            "Central Processor Unit",
-        ],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question:
-            "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
-        correct_answer: "Final",
-        incorrect_answers: ["Static", "Private", "Public"],
-    },
-    {
-        category: "Science: Computers",
-        type: "boolean",
-        difficulty: "easy",
-        question: "The logo for Snapchat is a Bell.",
-        correct_answer: "False",
-        incorrect_answers: ["True"],
-    },
-    {
-        category: "Science: Computers",
-        type: "boolean",
-        difficulty: "easy",
-        question:
-            "Pointers were not used in the original C programming language; they were added later on in C++.",
-        correct_answer: "False",
-        incorrect_answers: ["True"],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question:
-            "What is the most preferred image format used for logos in the Wikimedia database?",
-        correct_answer: ".svg",
-        incorrect_answers: [".png", ".jpeg", ".gif"],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "In web design, what does CSS stand for?",
-        correct_answer: "Cascading Style Sheet",
-        incorrect_answers: [
-            "Counter Strike: Source",
-            "Corrective Style Sheet",
-            "Computer Style Sheet",
-        ],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question:
-            "What is the code name for the mobile operating system Android 7.0?",
-        correct_answer: "Nougat",
-        incorrect_answers: [
-            "Ice Cream Sandwich",
-            "Jelly Bean",
-            "Marshmallow",
-        ],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "On Twitter, what is the character limit for a Tweet?",
-        correct_answer: "140",
-        incorrect_answers: ["120", "160", "100"],
-    },
-    {
-        category: "Science: Computers",
-        type: "boolean",
-        difficulty: "easy",
-        question: "Linux was first created as an alternative to Windows XP.",
-        correct_answer: "False",
-        incorrect_answers: ["True"],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question:
-            "Which programming language shares its name with an island in Indonesia?",
-        correct_answer: "Java",
-        incorrect_answers: ["Python", "C", "Jakarta"],
-    },
-];
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#39;t get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+]
 
 window.onload = function () {
-    // TIPS:
-
-    // SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
-    // Per ogni domanda, crea un container e incorporale tutte all'interno. 
-    // Crea poi dei radio button
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-    // con le risposte corrette e incorrette come opzioni
-    // (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale) 
-    //
-    // SE MOSTRI UNA DOMANDA ALLA VOLTA:
-    // Mostra la prima domanda con il testo e i radio button.
-    // Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
-    // salvando le risposte dell'utente in una variabile
-};
+  // TIPS:
+  // SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
+  // Per ogni domanda, crea un container e incorporale tutte all'interno.
+  // Crea poi dei radio button
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+  // con le risposte corrette e incorrette come opzioni
+  // (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale)
+  //
+  // SE MOSTRI UNA DOMANDA ALLA VOLTA:
+  // Mostra la prima domanda con il testo e i radio button.
+  // Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
+  // salvando le risposte dell'utente in una variabile
+}
 
 // Come calcolare il risultato? Hai due strade:
 // Se stai mostrando tutte le domande nello stesso momento, controlla semplicemente se i radio button selezionati sono === correct_answer
@@ -156,25 +151,24 @@ let indiceDomande = 0
 let intervalId = null
 let punteggio = 0 //variabile dove ciclare il punteggio ottenuto
 
-const countDownElement = document.getElementById('timer')
+const countDownElement = document.getElementById("timer")
 
 const timer = function () {
-    if (intervalId) {
-        clearInterval(intervalId)
-    }
+  if (intervalId) {
+    clearInterval(intervalId)
+  }
 
-    countDownTime = 20
+  countDownTime = 20
+  countDownElement.textContent = countDownTime
+
+  intervalId = setInterval(function () {
+    countDownTime--
     countDownElement.textContent = countDownTime
-
-
-    intervalId = setInterval(function () {
-        countDownTime--
-        countDownElement.textContent = countDownTime
-        if (countDownTime <= 0) {
-            clearInterval(intervalId)
-            intervalId = null
-            nextQuestion() //PASSA AUTOMATICAMENTE ALLA PROSSIMA DOMANDA
-            /* indiceDomande++
+    if (countDownTime <= 0) {
+      clearInterval(intervalId)
+      intervalId = null
+      nextQuestion() //PASSA AUTOMATICAMENTE ALLA PROSSIMA DOMANDA
+      /* indiceDomande++
  
              if (indiceDomande >= questions.length) {
                  countDownElement.textContent = "Quiz terminato"
@@ -183,83 +177,80 @@ const timer = function () {
              }
              currentQuestion()
              timer()*/
-        }
-    }, 1000)
-
+    }
+  }, 1000)
 }
 timer()
 
 //FUNZIONI DOMANDE
 
 const nextQuestion = function () {
-    const risposte = document.getElementsByName("risposta")
-    let rispostaSelezionata = ""
+  const risposte = document.getElementsByName("risposta")
+  let rispostaSelezionata = ""
 
-    for (let i = 0; i < risposte.length; i++) {
-        if (risposte[i].checked) {
-            rispostaSelezionata = risposte[i].value
-            break
-        }
+  for (let i = 0; i < risposte.length; i++) {
+    if (risposte[i].checked) {
+      rispostaSelezionata = risposte[i].value
+      break
     }
+  }
 
-    if (rispostaSelezionata === questions[indiceDomande].correct_answer) {
-        punteggio++;
-    }
-    console.log("Punteggio:", punteggio)
+  if (rispostaSelezionata === questions[indiceDomande].correct_answer) {
+    punteggio++
+  }
+  console.log("Punteggio:", punteggio)
 
-    indiceDomande++
-    if (indiceDomande < questions.length) {
-        currentQuestion()
-        timer()
-    } else {
-        localStorage.setItem("punteggioFinale", punteggio)
-        window.location.href = "result.html"
-        
-        localStorage.setItem("numeroDomande", questions.length)
-    }
+  indiceDomande++
+  if (indiceDomande < questions.length) {
+    currentQuestion()
+    timer()
+  } else {
+    localStorage.setItem("punteggioFinale", punteggio)
+    window.location.href = "result.html"
+
+    localStorage.setItem("numeroDomande", questions.length)
+  }
 }
 
 let arrayDiRisposte = []
 
 const currentQuestion = function () {
+  const quizContainer = document.getElementById("domande")
+  quizContainer.innerHTML = ""
 
-    const quizContainer = document.getElementById("domande")
-    quizContainer.innerHTML = ""
+  const domanda = questions[indiceDomande].question
+  const titoloDomande = document.createElement("h2")
+  titoloDomande.innerText = domanda
 
-    const domanda = questions[indiceDomande].question
-    const titoloDomande = document.createElement("h2")
-    titoloDomande.innerText = domanda
+  const resultContainer = document.getElementById("risposte-div")
+  resultContainer.innerHTML = ""
 
-    const resultContainer = document.getElementById("risposte-div")
-    resultContainer.innerHTML = ""
+  const arrayDiRisposte = [
+    questions[indiceDomande].correct_answer,
+    ...questions[indiceDomande].incorrect_answers,
+  ]
+  arrayDiRisposte.sort(() => Math.random() - 0.5) // mescola le risposte
 
-    const arrayDiRisposte = [questions[indiceDomande].correct_answer, ...questions[indiceDomande].incorrect_answers]
-    arrayDiRisposte.sort(() => Math.random() - 0.5) // mescola le risposte
-
-    const risposte = document.createElement("div")
-    for (let i = 0; i < arrayDiRisposte.length; i++) {
-        risposte.innerHTML += `
+  const risposte = document.createElement("div")
+  for (let i = 0; i < arrayDiRisposte.length; i++) {
+    risposte.innerHTML += `
     
     <input type="radio" onclick="nextQuestion()" name="risposta" id="bottone_risposta${i}" value="${arrayDiRisposte[i]}" />
     <label for="bottone_risposta${i}">${arrayDiRisposte[i]}</label>
       
     `
-    }
+  }
 
+  resultContainer.appendChild(risposte)
 
-    resultContainer.appendChild(risposte)
+  quizContainer.appendChild(titoloDomande)
+  const counterQuestion = document.getElementById("p_question")
 
-    quizContainer.appendChild(titoloDomande)
-    const counterQuestion = document.getElementById("p_question")
-
-    counterQuestion.innerText = `QUESTION ${indiceDomande + 1} / ${questions.length}`
-
+  counterQuestion.innerText = `QUESTION ${indiceDomande + 1} / ${
+    questions.length
+  }`
 }
-
 
 currentQuestion()
 
 //FUNZIONI RISPOSTE
-
-
-
